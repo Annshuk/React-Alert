21f354e55cb9443069cc516c5bd1c3e8e3c71230
Annshuk <annshuks@gmail.com>
2022-08-18 14:21:05 +0530

    1 file modified

--- src/UnorderList.js
+++ src/UnorderList.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { useSpring, animated } from 'react-spring';
 
 const UnorderList = () => {
+  const [styles, api] = useSpring(() => ({ opacity: 1 }));
+
   return (
-    <ul>
-      <li>text 1</li>
-      <li>text 1</li>
-      <li>text 1</li>
-      <li>text 1</li>
-      <li>text 1</li>
-      <li>text 1</li>
-    </ul>
+    <animated.div style={styles}>
+      <ul>
+        <li>text 1</li>
+        <li>text 1</li>
+        <li>text 1</li>
+        <li>text 1</li>
+        <li>text 1</li>
+        <li>text 1</li>
+      </ul>
+    </animated.div>
   );
 };
 
